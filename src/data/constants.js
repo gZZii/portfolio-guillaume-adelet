961564cc4233d56513c4a777043b3e8138ddbaa0
gZii <guillaume.adelet@gmail.com>
2024-03-17 11:15:45 +0100

    Portfolio en attente de déploiement

---
+++ src/data/constants.js
@@ -0,0 +1,209 @@
+export const Bio = {
+  name: "Guillaume ADELET",
+  roles: ["Web Devellopeur"],
+  description:
+    "I am a motivated and versatile individual, eager to take on new challenges and dedicated to delivering high-quality results. With a passion for learning and a growth mindset, I am ready to make a significant impact and achieve great things.",
+  github: "https://github.com/gZZii",
+  resume:
+    "https://drive.google.com/file/d/1Iu6qW5JeyvwTOx1W8lfJlzlxKgcm05CD/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/guillaume-adelet-5a95851a8/",
+  twitter: "https://twitter.com/Striky64",
+  insta: "https://www.instagram.com/guillaume_adlt/",
+  facebook: "https://www.facebook.com/guillaume.adelet",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        alt: "React Js logo",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        alt: "HTML logo",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        alt: "CSS logo",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        alt: "JavaScript logo",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+        alt: "Node Js logo",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://w7.pngwing.com/pngs/925/447/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo.png",
+        alt: "Express Js logo",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        alt: "MongoDB logo",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://www.e-genieclimatique.com/wordpress/wp-content/uploads/2021/05/Git-Icon-1788C.png",
+        alt: "Git logo",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        alt: "GitHub logo",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        alt: "VS Code logo",
+      },
+      {
+        name: "Postman",
+        image: "https://cdn.worldvectorlogo.com/logos/postman.svg",
+        alt: "Postman logo",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg",
+        alt: "Figma logo",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://e7.pngegg.com/pngimages/983/459/png-clipart-no-symbol-computer-icons-symbol-miscellaneous-angle.png",
+    alt: "No symbol",
+    role: "None... for the moment",
+    company: "Still looking for a company to hire me",
+    date: "Since April 2024",
+    desc: "",
+    skills: [
+      "Resume Submitter",
+      "Job Ad Seeker",
+      "Responding to Inquiries",
+      "Completion of Personal Projects",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/fr/0/0d/Logo_OpenClassrooms.png",
+    alt: "OpenClassrooms logo",
+    school: "Web Developpeur - OpenClassrooms",
+    date: "2023-2024",
+    desc: "During this training, I learned to build responsive websites using HTML, CSS, JavaScript, and React. I also managed to create an API and databases with NodeJS, Express, and MongoDB for dynamic sites. Additionally, I optimized performance, maintained websites, and improved their SEO. Finally, I was introduced to complete web project management (Scrum methodology), from planning to client presentations, including technical specifications.",
+    degree: "Diploma Advanced Technician",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Kasa",
+    date: "January 2024",
+    description:
+      "Kasa is a leading platform in France for peer-to-peer apartment rentals.  transitioning from a legacy ASP.NET framework to a modern JavaScript stack, with NodeJS for the backend and React for the frontend. The project focuses on developing a React application, incorporating React components and routes based on responsive Figma mockups",
+    image:
+      "https://thomaslecam.com/assets/kasa_xl-02f59cf6.webp",
+    alt: "Kasa website image",
+    tags: [
+      "React Js",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    category: "web app",
+    github: "https://github.com/gZZii/kasa",
+  },
+  {
+    id: 0,
+    title: "Mon vieux grimoire",
+    date: "February 2024",
+    description:
+      "`Mon Vieux Grimoire` is a new book rating website. It allows members to add books and rate them publicly. The site, developed in collaboration between a front-end and a back-end developer, features a React-based front-end and an API-focused back-end. The back-end also includes image optimization for user-uploaded images to adhere to Green Code practices. This platform aims to enhance the book selection and purchasing experience for readers by providing a community-driven rating system.",
+    image:
+      "https://romain-goujon.com/images/portfolio/mon_vieux_grimoire/accueil.webp",
+    alt: "Mon vieux grimoire website image",
+    tags: [, "MongoDb", "Node Js", "Express Js", "Multer" ],
+    category: "web app",
+    github: "https://github.com/gZZii/P7-Dev-Web-livres",
+  },
+  {
+    id: 1,
+    title: "Sophie Bluel Portfolio",
+    date: "September 2023",
+    description:
+      "The project involves developing a website for an interior architect's portfolio. As a front-end developer, your main tasks include creating a presentation page showcasing the architect's work, developing an administrator login page, and designing a modal for uploading new media. These components will be built using the provided design mockups on Figma, along with integrating with backend code for data persistence and functionality testing, supported by a Kanban board for task management.",
+    image:
+      "https://www.cams-code-projects.com/img-projects/sophie-bluel/sophie-bluel-picture-1.webp",
+    alt: "Sophie Bluel website image",
+    tags: [
+      "Javascript",
+      "HTML",
+      "CSS",
+      "Figma",
+    ],
+    category: "portfolio",
+    github: "https://github.com/gZZii/P3",
+    webapp: "https://vexa-app.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Nina Carducci Portfolio",
+    date: "October 2023",
+    description:
+      "The project involves a comprehensive optimization of Nina Carducci's photography website, focusing on performance enhancements, SEO, and local search visibility using Schema.org. Additionally, it includes social media meta tags, accessibility improvements, and bug fixes in the gallery modal navigation and image filter selection. The goal is to increase site speed, improve search engine rankings, and enhance user experience, culminating in a detailed optimization report outlining actions taken and their impacts.",
+    image:
+      "https://nicolas-vivier.com/static/media/SEO.33d0a2e4ffa656e3e9e7.webp",
+    alt: "Nina Carducci website image",
+    tags: ["HTML", "CSS", "Javascript", " WAVE", "Lighthouse",],
+    category: "portfolio",
+    github: "https://github.com/gZZii/P5",
+  },
+  {
+    id: 3,
+    title: "Booki",
+    date: "July 2023",
+    description:
+      "The project is for Booki, a startup aiming to develop a website for users to find accommodations and activities in their chosen city. As a web development intern, you're tasked with integrating the site's interface using HTML and CSS, based on provided desktop, tablet, and mobile mockups. You'll work closely with the CTO, Sarah, and UI designer, Loïc, using a structured file setup and a codebase that includes a preliminary `index.html`, a `css` folder with initial styles, and an `images` folder. Your goal is to complete and refine this setup, maintaining the provided directory structure and utilizing Git for version control.",
+    image:
+      "https://user-images.githubusercontent.com/93211301/157113398-261b05fe-9007-4188-9b74-34749a895629.png",
+    alt: "Booki website image",
+    tags: ["HTML", "CSS", "Git", "GitHub",],
+    category: "web app",
+    github: "https://github.com/gZZii/P2",
+  },
+];
